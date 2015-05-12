7b9f18de71bf849de2dbf12e6e7a63c0590daaca
Chad Elofson <chad.elofson@gmail.com>
2015-05-12 00:53:27 -0700

    First commit for the project

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+
+    name='vbs2py3',
+    version='1.0',
+    py_modules=['vbs2py3','Tkinter'],
+
+    # metadata
+    author=['Chad Elofson', 'Brad Henness'],
+    description='A program to convert vbs code to python 3 code.',
+    license='',
+    keywords=''
+)
